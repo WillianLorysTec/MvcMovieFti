5baaac394ac4c55f5d41fc5859278b1512148996
Willian Lorys <Loryswill@outlook.com>
2021-12-17 11:09:59 -0300

    Adicionar arquivos de projeto.

---
+++ MvcMovieFti/Data/MvcMovieFtiContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieFti.Models;
+
+namespace MvcMovieFti.Data
+{
+    public class MvcMovieFtiContext : DbContext
+    {
+        public MvcMovieFtiContext (DbContextOptions<MvcMovieFtiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieFti.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MvcMovieFti/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovieFti.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime CreatedDateMovie { get; set;}
+
+        [Required]
+        [DataType(DataType.Date)] 
+        public DateTime CreatedDateBD { get; set; }
+
+        [Required]
+        public int DurationInMinutes { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
